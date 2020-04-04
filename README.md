8e3086557b0f25d1223602be77dd719e0108a815
root <root@YLT160003.corp.optym.net>
2020-04-05 00:45:11 +0400

    Deployment first, coding second: avoid 550 step to production!

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # unicorn
-unicorn startup, spring cloud with redis cache, zipkin and zuul
 
+unicorn startup, spring cloud with redis, cache, zipkin and zuul
+
+./run.sh
+
+Critical links:
+
+1. Zipkin http://localhost:9411/zipkin/dependency
+2. Eureka http://localhost:8761
+3. Weather http://localhost:2030/swagger-ui.html
+4. Location http://localhost:2031/swagger-ui.html
+5. Cache http://localhost:2032/swagger-ui.html
+6. Web http://localhost:9595/index.jsf
 
 ![Image description](Unicorn.jpg)
--- cache/src/main/java/io/project/app/cache/CacheApplication.java
+++ cache/src/main/java/io/project/app/cache/CacheApplication.java
@@ -1,21 +1,38 @@
 package io.project.app.cache;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient
 @Slf4j
 public class CacheApplication {
-  
 
     public static void main(String[] args) {
-        SpringApplication.run(CacheApplication.class, args);
-      
+        final SpringApplication application = new SpringApplication(CacheApplication.class);
+        application.setBannerMode(Banner.Mode.CONSOLE);
+        application.setWebApplicationType(WebApplicationType.REACTIVE);
+        application.run(args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+
     }
 
 }
